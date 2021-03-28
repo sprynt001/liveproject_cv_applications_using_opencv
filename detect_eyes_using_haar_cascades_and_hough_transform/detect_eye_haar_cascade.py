e76bcb4af3fa0f6c733f2078f0cedbee300a5ccf
Kola <kola@sprynt.com>
2021-03-28 13:42:09 +0100

    Add functionality to write out processed image

--- detect_eyes_using_haar_cascades_and_hough_transform/detect_eye_haar_cascade.py
+++ detect_eyes_using_haar_cascades_and_hough_transform/detect_eye_haar_cascade.py
@@ -2,16 +2,22 @@
 
 """
 
+import os
 import cv2
 from argparse import ArgumentParser
 
 parser = ArgumentParser("Detecting eyes in images using haar cascades")
 parser.add_argument("-i", "--input-path", required=True,
                     help="path to image to process")
+parser.add_argument("-o", "--output-path", default=None,
+                    help="path to save image with bounding box " + \
+                         "if not supplied appends '_eyes' to " +\
+                         "input file name")
 
 args = parser.parse_args()
 
 input_path = args.input_path
+output_path = args.output_path
 
 image = cv2.imread(input_path)
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -25,3 +31,11 @@ for (x,y,w,h) in eyes:
 cv2.imshow('image', image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+# Save the output to file
+if output_path == None:
+    dirname, fname = os.path.split(input_path)
+    fbase, fext = os.path.splitext(fname)
+    output_path = os.path.join(dirname, fbase+"_eyes"+fext)
+cv2.imwrite(output_path, image)
+print(f"Written processed image to {output_path}")
